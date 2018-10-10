7e49d224e52a797502bf36726244d6a1d87a83b8
Lauren Piontek <piontekle@gmail.com>
2018-10-10 21:37:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,39 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+}
+
+function appendKitten(name) {
+  var kittens = [...kittens, name];
+  return kittens
+}
+
+function prependKitten(name) {
+ kittens = [name, ...kittens]
+  return kittens
+}
+
+function removeLastKitten() {
+  kittens.slice(-1)
+  return kittens
+}
+
+function removeFirstKitten() {
+  kittens.slice(1)
+  return kittens
+}
